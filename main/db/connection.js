31d6ccc47b0fc54c65a591a16c0d5dbee0885705
jamu-bu <jamu@bu.edu>
2023-10-16 00:21:37 -0700

    created connection.js and updated department.js

---
+++ main/db/connection.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql2");
+
+require('dotenv').config()
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME
+  },
+  console.log("Connected to the employee database.")
+);
+
+module.exports = db;
