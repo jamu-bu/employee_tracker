1c5411e664842d3e0ae31a35e51b87557c3c4fff
jamu-bu <jamu@bu.edu>
2023-10-17 22:13:20 -0700

    corrected file path in server.js

--- main/server.js
+++ main/server.js
@@ -1,6 +1,5 @@
 const db = require("./db/connection");
 const express = require("express");
-const startInquirer = require("./lib/departments");
 const runInquirer = require("./lib/department");
 const PORT = process.env.PORT || 3001;
 const app = express();
